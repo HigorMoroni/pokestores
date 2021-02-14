62040eba2de4dbf05a4756ca12049da5cc06741c
Higor Moroni <higor.moroni@prodigious.com>
2021-02-13 23:54:55 -0300

    Iniciando os componentes

---
+++ src/components/DetailBase.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DetailBase() {
+  return (
+    <h1>Detalhe</h1>
+  )
+}
+
+export default DetailBase;
---
+++ src/components/StoreBase.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StoreBase() {
+  return (
+    <h1>Loja</h1>
+  )
+}
+
+export default StoreBase;
