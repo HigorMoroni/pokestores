499e2c148f7342bd393d4c096f14d8fafe5a922e
Higor Moroni <higor.moroni@prodigious.com>
2021-02-17 01:09:12 -0300

    Finalizando e Estilizando a home

--- src/pages/Pokestores.tsx
+++ src/pages/Pokestores.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-import logo from '../images/logo-pokestores.jpg';
+import Storecard from '../components/Storecard';
+
+import PokéStores from '../images/logo-pokestores.jpg';
+import storesJson from '../json/stores.json';
+
+import '../styles/pages/pokestores.scss';
 
 function Pokestores() {
   return (
-    <img src={logo} alt="PokéStores" />
+    <section id="pokestores">
+      <header>
+        <img src={PokéStores} alt="PokéStores"/>
+        <h2>As melhores lojas do seu Pokémon na internet</h2>
+      </header>
+      <div className="cards-list">
+        {storesJson.map(({ id, name }) => {
+          return <Storecard key={id} id={id} name={name} />
+        })}
+      </div>
+      <footer>
+        <img src={PokéStores} alt="PokéStores"/>
+        <span>As melhores lojas do seu Pokémon na internet</span>
+      </footer>
+    </section>
   )
 }
 
